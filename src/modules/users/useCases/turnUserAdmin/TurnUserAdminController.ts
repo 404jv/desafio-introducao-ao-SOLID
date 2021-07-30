64a4f0415ab1de39932d23a385438656e87a6f19
João Victor <joaovictorramalho7@gmail.com>
2021-07-30 20:25:50 -0300

    Implements controller to turn a user as Admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,18 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    let user: User;
+
+    try {
+      user = this.turnUserAdminUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(404).json({
+        error: error.message,
+      });
+    }
+
+    return response.json(user);
   }
 }
 
